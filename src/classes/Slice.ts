f009a1aaf1061a9e1e11741aea724d8273ca7505
osoclos <osoclosus@gmail.com>
2026-03-03 00:38:54 +0800

    added strings and byte slices (binary)

---
+++ src/classes/Slice.ts
@@ -0,0 +1,199 @@
+import { MpClassImpl, MpClassInterface, MpClassModule } from "../types";
+import { toLegible } from "../utils";
+
+export const Slice = class Slice implements MpClassInterface<Uint8Array> {
+    #data: Uint8Array;
+
+    #nullable: boolean;
+    #isNull: boolean;
+
+    /** Wraps byte data for MessagePack parsing.
+     *
+     * @default new Uint8Array()
+     *
+     * @example new Slice(); // creates a byte data wrapper and defaults to `[]`
+     * @example new Slice(new Uint8Array([0x01, 0x02, 0x03])); // wraps byte data containing (`0x01`, `0x02`, `0x03`)
+     *
+     */
+    constructor(data?: Uint8Array);
+
+    /** Interprets the first bytes (derived from `Uint8Array.byteOffset`) from a buffer to a MessagePack byte data wrapper.
+     *
+     * @example new Slice(new Uint8Array([0x01, 0x02, 0x03])); // interprets the bytes (`0x01`, `0x02`, `0x03`) as `[0x01, 0x02, 0x03]`
+     * @example new Slice(new Uint8Array([])); // interprets an empty buffer as `[]`
+     *
+     */
+    constructor(bfr: Uint8Array);
+
+    constructor(a: Uint8Array = new Uint8Array()) {
+        this.#nullable = false;
+        this.#isNull = false;
+
+        if (a instanceof Uint8Array) {
+            const bfr = a;
+
+            this.#data = bfr;
+            return;
+        }
+
+        const data = a;
+
+        if (Slice.isRawValid(data)) this.#data = data;
+        else throw new TypeError(`Invalid value was passed into \`Slice\`. Did not expect ${toLegible(data)}.`);
+    }
+
+    /** Wraps nullable byte data for MessagePack parsing.
+     *
+     * @default null
+     *
+     * @example Slice.nullable(); // creates a nullable byte data wrapper and defaults to `null`
+     * @example Slice.nullable(new Uint8Array([0x01, 0x02, 0x03])); // wraps byte data containing (`0x01`, `0x02`, `0x03`)
+     *
+     */
+    static nullable(data?: Uint8Array | null): MpClassInterface<Uint8Array | null>;
+
+    /** Interprets the first bytes (derived from `Uint8Array.byteOffset`) from a buffer to a MessagePack nullable byte data wrapper.
+     *
+     * @example new Slice(new Uint8Array([0x01, 0x02, 0x03])); // interprets the bytes (`0x01`, `0x02`, `0x03`) as `[0x01, 0x02, 0x03]`
+     * @example new Slice(new Uint8Array([])); // interprets an empty buffer as `null`
+     *
+     */
+    static nullable(bfr: Uint8Array): MpClassInterface<Uint8Array | null>;
+
+    static nullable(a: Uint8Array | null | Uint8Array = null): MpClassInterface<Uint8Array | null> {
+        let isNull: boolean;
+
+        if (a instanceof Uint8Array) isNull = false;
+        else {
+            const data = a;
+            isNull = Object.is(data, null);
+        }
+
+        const slice = isNull ? new Slice() : new Slice(<Uint8Array>a);
+        slice.#nullable = true;
+        slice.#isNull = isNull;
+
+        return slice;
+    }
+
+    /** Retrieves the wrapped byte data values. */
+    raw(): Uint8Array;
+
+    /** Sets new byte data values and wrap it. */
+    raw(data: Uint8Array): void;
+
+    raw(data?: Uint8Array): Uint8Array | void {
+        if (data === undefined && arguments.length === 0) return this.#nullable && this.#isNull ? <Uint8Array><unknown>null : this.#data;
+
+        if (this.#nullable && Object.is(data, null)) this.#isNull = true;
+
+        if (Slice.isRawValid(data)) {
+            this.#data = data;
+            this.#isNull = false;
+        } else throw new TypeError(`Invalid value was passed into \`Slice\`. Did not expect ${toLegible(data)}.`);
+    }
+
+    /** Encodes the wrapped byte data and converts it to a MessagePack chunk. */
+    encode(): Uint8Array {
+        if (this.#nullable && this.#isNull) return new Uint8Array([0xc0]);
+
+        let code: number;
+        let lenLen: number;
+
+        const bytes = this.#data;
+        const len = bytes.byteLength;
+
+        switch (true) {
+            case len <= 0xff: {
+                code = 0xc4;
+                lenLen = 1;
+
+                break;
+            }
+
+            case len <= 0xffff: {
+                code = 0xc5;
+                lenLen = 2;
+
+                break;
+            }
+
+            default: {
+                code = 0xc6;
+                lenLen = 4;
+
+                break;
+            }
+        }
+
+        const iDataStart = 1 + lenLen;
+
+        const bfr = new Uint8Array(iDataStart + len);
+        bfr[0] = code;
+
+        let tmpByteLen = len;
+        for (let i: number = 1; i <= lenLen; i++) {
+            bfr[i] = tmpByteLen & 0xff;
+            tmpByteLen >>>= 8;
+        }
+
+        bfr.set(bytes, iDataStart);
+
+        return bfr;
+    }
+
+    /** Decodes a byte data MessagePack chunk, validates it and parses it to a Slice. */
+    static decode(chunk: Uint8Array): Slice {
+        const code = chunk[chunk.byteOffset];
+        if (code === undefined) throw new Error("Unable to retrieve header code from `chunk`. Is the chunk empty/truncated or `chunk.byteOffset` exceeded its length?");
+
+        let lenLen: number;
+        switch (code) {
+            case 0xc4: {
+                lenLen = 1;
+                break;
+            }
+
+            case 0xc5: {
+                lenLen = 2;
+                break;
+            }
+
+            case 0xc6: {
+                lenLen = 4;
+                break;
+            }
+
+            default: throw new TypeError(`Invalid chunk header for \`Slice\`. Did not expect ${toLegible(code, true)}.`);
+        }
+
+        let iDataStart = chunk.byteOffset + 1;
+
+        let len: number = 0;
+        for (let i: number = iDataStart, nBytes = 0; i < chunk.byteLength && nBytes < lenLen; i++, nBytes++) len |= chunk[i]! << (8 * nBytes);
+
+        iDataStart += lenLen;
+
+        const iDataEnd = iDataStart + len;
+        if (iDataEnd > chunk.byteLength) console.warn("Chunk buffer has insufficient data to be decoded. Was the chunk truncated?");
+
+        return new Slice(chunk.slice(iDataStart, iDataEnd));
+    }
+
+    /** Checks whether a value is valid for a Slice. */
+    static isRawValid(data: any): data is Uint8Array {
+        return data instanceof Uint8Array;
+    }
+
+    /** Checks whether a chunk header code is valid for a Slice. */
+    static isCodeValid(code: number): boolean {
+        return (
+            code === 0xc4 ||
+            code === 0xc5 ||
+            code === 0xc6
+        );
+    }
+
+    /** Checks whether a chunk is valid for a Slice. */
+    static isChunkValid = MpClassImpl.isChunkValid.bind(Slice);
+} satisfies MpClassModule<Uint8Array>;
---
+++ src/classes/Str.ts
@@ -0,0 +1,220 @@
+import { MpClassImpl, MpClassInterface, MpClassModule } from "../types";
+import { toLegible } from "../utils";
+
+export const Str = class Str implements MpClassInterface<string> {
+    #data: string;
+
+    #nullable: boolean;
+    #isNull: boolean;
+
+    static #encoder = new TextEncoder();
+    static #decoder = new TextDecoder("utf-8", { fatal: true });
+
+    /** Wraps a string for MessagePack parsing.
+     *
+     * @default ""
+     *
+     * @example new Str(); // creates a string wrapper and defaults to `""`
+     * @example new Str("foo"); // wraps a "foo" string
+     *
+     */
+    constructor(data?: string);
+
+    /** Interprets the first bytes (derived from `Uint8Array.byteOffset`) from a buffer to a MessagePack string wrapper.
+     *
+     * @example new Str(new Uint8Array([0x41, 0x42, 0x43])); // interprets the bytes (`0x41`, `0x42`, `0x43`) as "ABC"
+     * @example new Str(new Uint8Array([])); // interprets an empty buffer as ""
+     *
+     */
+    constructor(bfr: Uint8Array);
+
+    constructor(a: string | Uint8Array = "") {
+        this.#nullable = false;
+        this.#isNull = false;
+
+        if (a instanceof Uint8Array) {
+            const bfr = a;
+
+            this.#data = Str.#decoder.decode(bfr);
+            return;
+        }
+
+        const data = a;
+
+        if (Str.isRawValid(data)) this.#data = data;
+        else throw new TypeError(`Invalid value was passed into \`Str\`. Did not expect ${toLegible(data)}.`);
+    }
+
+    /** Wraps a nullable string for MessagePack parsing.
+     *
+     * @default null
+     *
+     * @example Str.nullable(); // creates a nullable string wrapper and defaults to `null`
+     * @example Str.nullable("foo"); // wraps a "foo" string
+     *
+     */
+    static nullable(data?: string | null): MpClassInterface<string | null>;
+
+    /** Interprets the first bytes (derived from `Uint8Array.byteOffset`) from a buffer to a MessagePack nullable string wrapper.
+     *
+     * @example Str.nullable(new Uint8Array([0x41, 0x42, 0x43])); // interprets the bytes (`0x41`, `0x42`, `0x43`) as "ABC"
+     * @example Str.nullable(new Uint8Array([])); // interprets an empty buffer as `null`
+     *
+     */
+    static nullable(bfr: Uint8Array): MpClassInterface<string | null>;
+
+    static nullable(a: string | null | Uint8Array = null): MpClassInterface<string | null> {
+        let isNull: boolean;
+
+        if (a instanceof Uint8Array) {
+            const bfr = a;
+
+            const byte = bfr[bfr.byteOffset];
+            isNull = byte === undefined;
+        } else {
+            const data = a;
+            isNull = Object.is(data, null);
+        }
+
+        const str = isNull ? new Str() : new Str(<string>a);
+        str.#nullable = true;
+        str.#isNull = isNull;
+
+        return str;
+    }
+
+    /** Retrieves the wrapped string value. */
+    raw(): string;
+
+    /** Sets a new string value and wrap it. */
+    raw(data: string): void;
+
+    raw(data?: string): string | void {
+        if (data === undefined && arguments.length === 0) return this.#nullable && this.#isNull ? <string><unknown>null : this.#data;
+
+        if (this.#nullable && Object.is(data, null)) this.#isNull = true;
+
+        if (Str.isRawValid(data)) {
+            this.#data = data;
+            this.#isNull = false;
+        } else throw new TypeError(`Invalid value was passed into \`Str\`. Did not expect ${toLegible(data)}.`);
+    }
+
+    /** Encodes the wrapped string and converts it to a MessagePack chunk. */
+    encode(): Uint8Array {
+        if (this.#nullable && this.#isNull) return new Uint8Array([0xc0]);
+
+        let code: number;
+        let lenLen: number;
+
+        const bytes = Str.#encoder.encode(this.#data);
+        const len = bytes.byteLength;
+
+        switch (true) {
+            case len < 0x20: {
+                code = 0xa0 | len;
+                lenLen = 0;
+
+                break;
+            }
+
+            case len <= 0xff: {
+                code = 0xd9;
+                lenLen = 1;
+
+                break;
+            }
+
+            case len <= 0xffff: {
+                code = 0xda;
+                lenLen = 2;
+
+                break;
+            }
+
+            default: {
+                code = 0xdb;
+                lenLen = 4;
+
+                break;
+            }
+        }
+
+        const iDataStart = 1 + lenLen;
+
+        const bfr = new Uint8Array(iDataStart + len);
+        bfr[0] = code;
+
+        let tmpByteLen = len;
+        for (let i: number = 1; i <= lenLen; i++) {
+            bfr[i] = tmpByteLen & 0xff;
+            tmpByteLen >>>= 8;
+        }
+
+        bfr.set(bytes, iDataStart);
+
+        return bfr;
+    }
+
+    /** Decodes a string MessagePack chunk, validates it and parses it to a Str. */
+    static decode(chunk: Uint8Array): Str {
+        const code = chunk[chunk.byteOffset];
+        if (code === undefined) throw new Error("Unable to retrieve header code from `chunk`. Is the chunk empty/truncated or `chunk.byteOffset` exceeded its length?");
+
+        let len: number;
+        let iDataStart = chunk.byteOffset + 1;
+
+        if ((code & 0xa0) === 0xa0) len = code & 0x1f;
+        else {
+            let lenLen: number;
+
+            switch (code) {
+                case 0xd9: {
+                    lenLen = 1;
+                    break;
+                }
+
+                case 0xda: {
+                    lenLen = 2;
+                    break;
+                }
+
+                case 0xdb: {
+                    lenLen = 4;
+                    break;
+                }
+
+                default: throw new TypeError(`Invalid chunk header for \`Str\`. Did not expect ${toLegible(code, true)}.`);
+            }
+
+            len = 0;
+            for (let i: number = iDataStart, nBytes = 0; i < chunk.byteLength && nBytes < lenLen; i++, nBytes++) len |= chunk[i]! << (8 * nBytes);
+
+            iDataStart += lenLen;
+        }
+
+        const iDataEnd = iDataStart + len;
+        if (iDataEnd > chunk.byteLength) console.warn("Chunk buffer has insufficient data to be decoded. Was the chunk truncated?");
+
+        return new Str(chunk.slice(iDataStart, iDataEnd));
+    }
+
+    /** Checks whether a value is valid for a Str. */
+    static isRawValid(data: any): data is string {
+        return typeof data === "string";
+    }
+
+    /** Checks whether a chunk header code is valid for a Str. */
+    static isCodeValid(code: number): boolean {
+        return (
+            (code & 0xa0) === 0xa0 ||
+
+            code === 0xd9 ||
+            code === 0xda ||
+            code === 0xdb
+        );
+    }
+
+    /** Checks whether a chunk is valid for a Str. */
+    static isChunkValid = MpClassImpl.isChunkValid.bind(Str);
+} satisfies MpClassModule<string>;
