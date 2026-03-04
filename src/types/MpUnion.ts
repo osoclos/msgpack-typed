7d47074cb5a598302072f67068600c36e45ba3d1
osoclos <osoclosus@gmail.com>
2026-03-04 12:38:05 +0800

    put generic decoding into loop

--- src/types/MpUnion.ts
+++ src/types/MpUnion.ts
@@ -2,3 +2,5 @@ import { Bool, BoolPrimitive, Flt, FltPrimitive, Int, IntPrimitive, Slice, Slice
 
 export type MpClassUnion = Uint | Int | Flt | Bool | Str | Slice;
 export type MpPrimitiveUnion = UintPrimitive | IntPrimitive | FltPrimitive | BoolPrimitive | StrPrimitive | SlicePrimitive | null;
+
+export const MP_CLASS_CONTAINER_UNION_LIST = [Uint, Int, Flt, Str, Bool, Slice] as const;
--- src/utils/decodeGeneric.ts
+++ src/utils/decodeGeneric.ts
@@ -1,20 +1,8 @@
-import { Bool, Flt, Int, Slice, Str, Uint } from "../classes";
-import { MpClassUnion } from "../types";
+import { MP_CLASS_CONTAINER_UNION_LIST, MpClassUnion } from "../types";
 
 export function decodeGeneric<T extends MpClassUnion | MpClassUnion[]>(chunk: Uint8Array): T {
-    switch (true) {
-        case Uint.isChunkValid(chunk): return <T>Uint.decode(chunk);
-        case Int.isChunkValid(chunk): return <T>Int.decode(chunk);
-
-        case Flt.isChunkValid(chunk): return <T>Flt.decode(chunk);
-
-        case Str.isChunkValid(chunk): return <T>Str.decode(chunk);
-        case Bool.isChunkValid(chunk): return <T>Bool.decode(chunk);
-
-        case Slice.isChunkValid(chunk): return <T>Slice.decode(chunk);
-
-        default: break;
-    }
+    for (const Cls of MP_CLASS_CONTAINER_UNION_LIST)
+        if (Cls.isChunkValid(chunk)) return <T>Cls.decode(chunk);
 
     throw new TypeError("Invalid data was passed as a MessagePack chunk.");
 }
