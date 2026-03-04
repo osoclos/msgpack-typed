65310215116c98154243f1bc5bad5f1723e408d5
osoclos <osoclosus@gmail.com>
2026-03-04 12:04:13 +0800

    added null as primitive

--- src/types/MpUnion.ts
+++ src/types/MpUnion.ts
@@ -1,4 +1,4 @@
 import { Bool, BoolPrimitive, Flt, FltPrimitive, Int, IntPrimitive, Slice, SlicePrimitive, Str, StrPrimitive, Uint, UintPrimitive } from "../classes";
 
 export type MpClassUnion = Uint | Int | Flt | Bool | Str | Slice;
-export type MpPrimitiveUnion = UintPrimitive | IntPrimitive | FltPrimitive | BoolPrimitive | StrPrimitive | SlicePrimitive;
+export type MpPrimitiveUnion = UintPrimitive | IntPrimitive | FltPrimitive | BoolPrimitive | StrPrimitive | SlicePrimitive | null;
