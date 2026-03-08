35b543df071c5253b9ffc1da9a154975c2d9f071
osoclos <osoclosus@gmail.com>
2026-03-08 11:20:51 +0800

    added extension encoding/decoding

--- src/utils/decodeGeneric.ts
+++ src/utils/decodeGeneric.ts
@@ -1,10 +1,125 @@
+import { Ext } from "../classes";
 import { ArrClassed, ObjClassed } from "../containers";
+
 import { MP_CLASS_CONTAINER_UNION_LIST, MpClassUnion } from "../types";
 
+import { toLegible } from "./toLegible";
+
 /** Decodes a MessagePack chunk with an arbitrary or unknown type to its corresponding class type. */
-export function decodeGeneric<T extends MpClassUnion | ArrClassed | ObjClassed>(chunk: Uint8Array): T {
+export function decodeGeneric<T extends MpClassUnion | ArrClassed | ObjClassed>(chunk: Uint8Array): T;
+
+/** Decodes a MessagePack chunk with an arbitrary or unknown type to its value or object, with an option to add extensions to the encoder. */
+export function decodeGeneric<T>(chunk: Uint8Array, exts?: Ext<any, number> | Ext<any, number>[]): T;
+
+export function decodeGeneric<T>(chunk: Uint8Array, exts: Ext<any, number> | Ext<any, number>[] = []): T {
     for (const Cls of MP_CLASS_CONTAINER_UNION_LIST)
         if (Cls.isChunkValid(chunk)) return <T>Cls.decode(chunk);
+
+    if (!Array.isArray(exts)) exts = [exts];
+
+    const iChunkStart = chunk.byteOffset;
+
+    const code = chunk[iChunkStart];
+    if (code === undefined) throw new Error("Unable to retrieve header code from `chunk`. Is the chunk empty/truncated or `chunk.byteOffset` exceeded its length?");
+
+    if (
+        code === 0xd4 ||
+        code === 0xd5 ||
+        code === 0xd6 ||
+        code === 0xd7 ||
+        code === 0xd8 ||
+
+        code === 0xc7 ||
+        code === 0xc8 ||
+        code === 0xc9
+    ) {
+        let len: number;
+        let lenLen: number;
+
+        switch (code) {
+            case 0xd4: {
+                len = 0x01;
+                lenLen = 0;
+
+                break;
+            }
+
+            case 0xd5: {
+                len = 0x02;
+                lenLen = 0;
+
+                break;
+            }
+
+            case 0xd6: {
+                len = 0x04;
+                lenLen = 0;
+
+                break;
+            }
+
+            case 0xd7: {
+                len = 0x08;
+                lenLen = 0;
+
+                break;
+            }
+
+            case 0xd8: {
+                len = 0x10;
+                lenLen = 0;
+
+                break;
+            }
+
+            case 0xc7: {
+                len = 0;
+                lenLen = 1;
+
+                break;
+            }
+
+            case 0xc8: {
+                len = 0;
+                lenLen = 2;
+
+                break;
+            }
+
+            case 0xc9: {
+                len = 0;
+                lenLen = 4;
+
+                break;
+            }
+
+            default: throw new TypeError(`Invalid chunk header for \`Ext\`. Did not expect ${toLegible(code, true)}.`);
+        }
+
+        const iLenStart = iChunkStart + 1;
+
+        for (let i: number = iLenStart, nBytes = 0; i < chunk.byteLength && nBytes < lenLen; i++, nBytes++) len |= chunk[i]! << (8 * nBytes);
+
+        const iType = iLenStart + lenLen;
+
+        const type = chunk[iType];
+        if (type === undefined) throw new Error("Unable to retrieve extension code from `chunk`. Is the chunk empty/truncated or `chunk.byteOffset` exceeded its length?");
+
+        let extInUse: Ext<any, number> | null = null;
+        for (const ext of exts) {
+            if (ext.isCodeValid(type)) {
+                extInUse = ext;
+                break;
+            }
+        }
+
+        if (extInUse === null) throw new Error(`No extension that  was passed into the decoder supports code ${toLegible(type, true)}. Did you add an extension that supports it?`);
+
+        const iDataStart = iType + 1;
+        const iDataEnd = iDataStart + len;
+
+        return extInUse.decode(chunk.slice(iDataStart, iDataEnd), type);
+    }
 
     throw new TypeError("Invalid data was passed as a MessagePack chunk.");
 }
--- src/utils/encodeGeneric.ts
+++ src/utils/encodeGeneric.ts
@@ -1,10 +1,105 @@
-import { Bool, Flt, Int, Slice, Str } from "../classes";
+import { Bool, Ext, Flt, Int, Slice, Str } from "../classes";
 import { Arr, Obj } from "../containers";
 
 import { MpClassUnion, MpPrimitiveUnion } from "../types";
 
 /** Encodes an arbitrary MessagePack class or primitive to a chunk buffer. */
-export function encodeGeneric(data: MpClassUnion | MpPrimitiveUnion): Uint8Array {
+export function encodeGeneric(data: MpClassUnion | MpPrimitiveUnion): Uint8Array;
+
+/** Encodes a value or object to a chunk buffer, with an option to add extensions to the encoder. */
+export function encodeGeneric(data: any, exts?: Ext<any, number> | Ext<any, number>[]): Uint8Array;
+
+export function encodeGeneric(data: any, exts: Ext<any, number> | Ext<any, number>[] = []): Uint8Array {
+    if (!Array.isArray(exts)) exts = [exts];
+
+    for (const ext of exts) {
+        if (!ext.isObjValid(data)) continue;
+
+        const res = ext.encode(data);
+
+        const bytes = Array.isArray(res) ? res[0] : res;
+        const type  = Array.isArray(res) ? res[1] : ext.codes[0]!;
+
+        let code: number;
+        let lenLen: number;
+
+        const len = bytes.byteLength;
+
+        switch (true) {
+            case len === 0x01: {
+                code = 0xd4;
+                lenLen = 0;
+
+                break;
+            }
+
+            case len === 0x02: {
+                code = 0xd5;
+                lenLen = 0;
+
+                break;
+            }
+
+            case len === 0x04: {
+                code = 0xd6;
+                lenLen = 0;
+
+                break;
+            }
+
+            case len === 0x08: {
+                code = 0xd7;
+                lenLen = 0;
+
+                break;
+            }
+
+            case len === 0x10: {
+                code = 0xd8;
+                lenLen = 0;
+
+                break;
+            }
+
+            case len <= 0xff: {
+                code = 0xc7;
+                lenLen = 1;
+
+                break;
+            }
+
+            case len <= 0xffff: {
+                code = 0xc8;
+                lenLen = 2;
+
+                break;
+            }
+
+            default: {
+                code = 0xc9;
+                lenLen = 4;
+
+                break;
+            }
+        }
+
+        const iDataStart = 1 + lenLen + 1;
+
+        const bfr = new Uint8Array(iDataStart + len);
+        bfr[0] = code;
+
+        let tmpLen = len;
+        for (let i: number = 1, nBytes: number = 0; nBytes < lenLen; i++, nBytes++) {
+            bfr[i] = tmpLen & 0xff;
+            tmpLen >>>= 8;
+        }
+
+        bfr[1 + lenLen] = type;
+        bfr.set(bytes, iDataStart);
+
+        return bfr;
+    }
+
     switch (true) {
         case typeof data === "number": {
             data = new Flt(data);
