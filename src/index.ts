8c000de490a5b55c6315fec2ac4bd94d1de3a930
osoclos <osoclosus@gmail.com>
2026-03-13 19:00:52 +0800

    added wat module exports

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export * from "./containers";
 
 export * from "./extensions";
 
+export * from "./modules";
+
 export { encodeGeneric, decodeGeneric, mpLz4Pack, mpLz4Unpack, encodeExtension, encodeExtensionRaw, decodeExtension, decodeExtensionRaw, deriveExtensionChunkRanges } from "./utils";
