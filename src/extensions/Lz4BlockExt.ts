5d41d5ed0e8ad549e253d42c13963dfc031501e0
osoclos <osoclosus@gmail.com>
2026-03-15 16:42:06 +0800

    added static create method to lz4 block ext

--- src/extensions/Lz4BlockExt.ts
+++ src/extensions/Lz4BlockExt.ts
@@ -1,5 +1,5 @@
 import { Ext } from "../classes";
-import { Lz4BlockModuleExports } from "../modules";
+import { Lz4BlockModuleExports, initMathModule, initHashTableModule, initLz4BlockModule } from "../modules";
 
 import { RawClass } from "../types";
 
@@ -7,6 +7,16 @@ export class Lz4BlockExt extends Ext<RawClass<unknown, any[]>, 0x62 | 0x63> {
     /** Creates an extension that adds compression and decompression to MessagePack chunks. Not to be used in isolation. */
     constructor(public lz4Block: Lz4BlockModuleExports, public maxBlockSize: number = 8192 /* 2 ^ 13 */) {
         super([0x62, 0x63], <RawClass<unknown, any[]>><unknown>null);
+    }
+
+    /** Creates an extension that adds compression and decompression to MessagePack chunks. Not to be used in isolation. */
+    static async create(maxBlockSize?: number, nHashBits?: number): Promise<Lz4BlockExt> {
+        const math = await initMathModule();
+        const hashTable = await initHashTableModule({ options: <any>{ nHashBits } });
+
+        const lz4Block = await initLz4BlockModule({ math, hashTable, debug: { log: console.log } });
+
+        return new Lz4BlockExt(lz4Block, maxBlockSize);
     }
 
     override encode(_data: unknown): Uint8Array {
