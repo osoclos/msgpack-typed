d49108f918ee9a4662571d74447248e4f58a9f66
osoclos <osoclosus@gmail.com>
2026-03-18 21:35:47 +0800

    archived src as reference

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./classes";
-export * from "./containers";
-
-export * from "./extensions";
-
-export * from "./modules";
-
-export { encodeGeneric, decodeGeneric, mpLz4Pack, mpLz4Unpack, ExtUtils } from "./utils";
