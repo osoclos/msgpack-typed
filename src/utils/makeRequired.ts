48409fa2ed69b578a8d4157c3eb630925aced8f0
osoclos <osoclosus@gmail.com>
2026-03-22 19:06:36 +0800

    added docs for converting wrapper's nullability

--- src/utils/makeRequired.ts
+++ src/utils/makeRequired.ts
@@ -1,5 +1,6 @@
 import { MpClassInterface } from "../types";
 
+/** Make a wrapper non-nullable (requires type coercion or assignment to another variable for correct type completion) */
 export function makeRequired<T>(wrapper: MpClassInterface<T, boolean>): MpClassInterface<T, false> {
     (<any>wrapper)["isOptional"] = false;
     wrapper.data = wrapper.default;
