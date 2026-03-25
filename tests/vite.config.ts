8a449ad1693762cd47616e6a60e7acf462b8d4e7
osoclos <osoclosus@gmail.com>
2026-03-26 00:02:19 +0800

    updated vite config to reliably use built files

--- tests/vite.config.ts
+++ tests/vite.config.ts
@@ -1,8 +1,11 @@
+import path from "path";
+
 import { defineConfig } from "vite";
 import watVitePlugin from "vite-plugin-wat2wasm";
 
 export default defineConfig({
     root: __dirname,
+    resolve: { alias: { "@": path.resolve(__dirname, "../dist") }},
 
     plugins: [
         watVitePlugin({
