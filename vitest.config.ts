be89aa770061ae8722d0aaff3ec0db639c61922b
osoclos <osoclosus@gmail.com>
2026-06-22 12:01:54 +0800

    updated vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
 
             provider: preview(),
             instances: [{ browser: "chromium" }],
-        }
+        },
+
+        passWithNoTests: true
     }
 });
